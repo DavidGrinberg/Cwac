4d732d32fe28ab65b889eea9102be394967762f9
David Grinberg <David@egrinberg.com>
2015-11-02 22:46:22 -0500

    testing git ignore

--- src/main/java/com/cwac/poc/test.java
+++ src/main/java/com/cwac/poc/test.java
@@ -10,27 +10,15 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by David on 10/24/2015.
  */
 public class test {
     public static void main(String[] args) {
-        final Morphia morphia = new Morphia();
-
-        // tell morphia where to find your classes
-        // can be called multiple times with different packages or classes
-        morphia.mapPackage("com.cwac.mongoDocs");
-
-        // create the Datastore connecting to the database running on the default port on the local host
-        final Datastore datastore = morphia.createDatastore(new MongoClient(), "cwac");
-        datastore.getDB().dropDatabase();
-        User firstUser = new User("david2", "nyc"),
-            secondUser = new User("matt", "nyc");
-        Set<User> users = new HashSet<>(Arrays.asList(firstUser, secondUser));
-        Meeting meeting = new Meeting(users, "nyc" );
-
-        datastore.save(users);
-        datastore.save(meeting);
+        String[] strings = {"a", "b", "c"};
+        Set<String> stringSet = Arrays.stream(strings).skip(1).collect(Collectors.toSet());
+        System.out.println(stringSet);
     }
 }
